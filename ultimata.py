cbbd5ee49c08451ac9df42d0a952eff18e3a860b
basil <basilcleveland@gmail.com>
2022-03-13 13:38:29 -0400

    Updated dimensions.

--- ultimata.py
+++ ultimata.py
@@ -1,8 +1,9 @@
 import sys, random
 import pygame as pg
 
+
 class ultimata:
-    def __init__(self, screen_size=(900, 600)): # dimensions for main game screen
+    def __init__(self, screen_size=(900, 600)):  # dimensions for main game screen
         pg.init()
         self.screen_size = screen_size
         self.screen = pg.display.set_mode(self.screen_size)
@@ -11,7 +12,7 @@ class ultimata:
         pg.display.set_caption(self.title)
         self.gridX = 30
         self.gridY = 20
-        self.cell_size = 30
+        self.cell_size = 32
         self.cell_count = self.gridX * self.gridY
         self.cells = {}
         self.create_cells()
@@ -19,7 +20,8 @@ class ultimata:
     def create_cells(self):
         for x in range(self.gridX):
             for y in range(self.gridY):
-                self.cells[(x, y)] = cell(x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size, self.screen)
+                self.cells[(x, y)] = cell(x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size,
+                                          self.screen)
 
     def main_loop(self):
         while True:
@@ -39,6 +41,7 @@ class ultimata:
         for cell in self.cells.values():
             cell.draw()
 
+
 class cell:
     def __init__(self, x, y, w, h, screen):
         self.screen = screen
@@ -48,6 +51,7 @@ class cell:
     def draw(self):
         pg.draw.rect(self.screen, self.color, self.rect)
 
-a = ultimata((900, 600))
+
+a = ultimata((1120, 800))
 a.main_loop()
 print("hi")
