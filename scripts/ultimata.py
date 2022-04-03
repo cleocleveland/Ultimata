e8a7ff92ad9b2e5ce19a93a827d7402e5bc25028
cleocleveland <cleomcleveland@gmail.com>
2022-04-03 11:15:41 -0400

    implemented solidity, starting position, and direction (wasd)

--- scripts/ultimata.py
+++ scripts/ultimata.py
@@ -3,10 +3,12 @@ import random
 import os
 import pygame as pg
 
+cells = {}
 tileset = {}
-tileinfo = {0:("wall"), 1:("wall"), 2:("wall"), 3:("wall"), 4:("floor"), 5:("floor"),
-            6:("water"), 7:("floor"), 8:("floor"), 9:("floor"), 10:("wall"), 11:("floor")}
-levels = {0: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+tileinfo = {0:("wall", "no pass"), 1:("wall", "no pass"), 2:("wall", "no pass"), 3:("wall", "no pass"),
+            4:("floor", "pass"), 5:("floor", "pass"), 6:("water", "no pass"), 7:("floor", "pass"),
+            8:("floor", "pass"), 9:("floor", "pass"), 10:("wall", "no pass"), 11:("floor", "pass")}
+levels = {0: ([4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
               0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0,
               0, 4, 4, 4, 4, 4, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 0, 4, 4, 4, 4, 0,
               0, 4, 4, 4, 4, 4, 0, 4, 4, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 0, 4, 4, 4, 4, 0,
@@ -21,7 +23,7 @@ levels = {0: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 
               4, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 4, 4, 4, 4, 4, 0, 0, 0, 0, 4, 4, 4, 4, 4, 0,
               4, 0, 4, 4, 4, 0, 0, 0, 0, 4, 4, 4, 4, 0, 4, 4, 0, 4, 4, 4, 4, 4, 0, 4, 4, 0, 4, 4, 4, 4, 4, 0,
               4, 0, 4, 4, 4, 0, 4, 4, 0, 0, 0, 0, 0, 0, 4, 4, 0, 4, 4, 4, 4, 4, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0,
-              4, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4]}
+              4, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4], (1, 2))}
 
 
 class Tile:
@@ -32,10 +34,10 @@ class Tile:
 
 class Ultimata:
     def __init__(self, screen_size=(1184, 672)):  # screen_size is tuple (dimensions for main game screen)
+        global levels
         pg.init()
         self.screen_size = screen_size
         self.screen = pg.display.set_mode(self.screen_size)
-        print(self.screen.get_size())
         self.title = "Ultimata"
         pg.display.set_caption(self.title)
         self.load_tiles()
@@ -44,9 +46,8 @@ class Ultimata:
         self.gridY = 16
         self.cell_size = 32
         self.cell_count = self.gridX * self.gridY
-        self.cells = {}
         self.create_cells()
-        self.player = Player(self.screen, self.cell_size, self.gridX, self.gridY)
+        self.player = Player(self.screen, self.cell_size, self.gridX, self.gridY, levels[self.current_level][1])
         self.font = pg.font.SysFont(None, 8, False, False)
         self.message_handler = MessageHandler(0, self.gridY * self.cell_size,
                                               (self.gridX + 5) * self.cell_size, (self.gridX + 5) * self.cell_size,
@@ -58,19 +59,18 @@ class Ultimata:
         for filename in os.listdir('tiles'):
             img = pg.image.load(os.path.join("tiles", filename)).convert()
             tileset[counter] = Tile(img)
-            print(filename)
             counter += 1
 
     def create_cells(self):
-        global levels
+        global levels, cells
         counter = 0
-        limit = len(levels[self.current_level])
+        limit = len(levels[self.current_level][0])
         for y in range(self.gridY):
             for x in range(self.gridX):
                 if counter >= limit:
                     counter = 0
-                tile = levels[self.current_level][counter]
-                self.cells[(x, y)] = Cell(x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size,
+                tile = levels[self.current_level][0][counter]
+                cells[(x, y)] = Cell(x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size,
                                           self.screen, tile)
                 counter += 1
 
@@ -97,6 +97,14 @@ class Ultimata:
                     self.player.move("right")
                 if event.key == pg.K_LEFT:
                     self.player.move("left")
+                if event.key == pg.K_w:
+                    self.player.direction = "up"
+                if event.key == pg.K_s:
+                    self.player.direction = "down"
+                if event.key == pg.K_a:
+                    self.player.direction = "right"
+                if event.key == pg.K_d:
+                    self.player.direction = "left"
 
     def screen_updater(self):
         self.cell_updater()
@@ -107,24 +115,28 @@ class Ultimata:
         pg.display.flip()
 
     def cell_updater(self):
-        for cell in self.cells.values():
+        global cells
+        for cell in cells.values():
             cell.draw()
 
 
 class Cell:
     def __init__(self, x, y, w, h, screen, tile):
+        global tileset, tileinfo
         self.screen = screen
         self.rect = pg.Rect(x, y, w, h)
         self.tile = tileset[tile]
+        self.tile_type = tileinfo[tile]
 
     def draw(self):
         self.screen.blit(self.tile.image, self.rect)
 
 
 class Player:
-    def __init__(self, surface, cell_size, gridX, gridY):
-        self.pos = (0, 0)
+    def __init__(self, surface, cell_size, gridX, gridY, start):
+        self.pos = start
         self.color = (100, 0, 100)
+        self.direction = ""
         self.surface = surface
         self.cell_size = cell_size
         self.offset = int(self.cell_size / 2)
@@ -133,17 +145,19 @@ class Player:
         self.gridY = gridY - 1
 
     def move(self, direction):
+        global cells
         if direction == "down":
-            if self.pos[1] != self.gridY:
+
+            if self.pos[1] != self.gridY and cells[(self.pos[0], self.pos[1] + 1)].tile_type[1] == "pass":
                 self.pos = (self.pos[0], self.pos[1] + 1)
         if direction == "up":
-            if self.pos[1] != 0:
+            if self.pos[1] != 0 and cells[(self.pos[0], self.pos[1] - 1)].tile_type[1] == "pass":
                 self.pos = (self.pos[0], self.pos[1] - 1)
         if direction == "right":
-            if self.pos[0] != self.gridX:
+            if self.pos[0] != self.gridX and cells[(self.pos[0] + 1, self.pos[1])].tile_type[1] == "pass":
                 self.pos = (self.pos[0] + 1, self.pos[1])
         if direction == "left":
-            if self.pos[0] != 0:
+            if self.pos[0] != 0 and cells[(self.pos[0] - 1, self.pos[1])].tile_type[1] == "pass":
                 self.pos = (self.pos[0] - 1, self.pos[1])
 
     def draw(self):
